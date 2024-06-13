254944355b7df810bcead2138cbdf1516c474fc6
Felix Zhou <felix.rn.zhou@gmail.com>
2024-06-12 23:02:01 -0400

    add login page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './core/auth/pages/login/login.component';
 
 export const routes: Routes = [
     {
-        path: '', component: HomeComponent
+        path: "", component: HomeComponent,
+    },
+    {
+        path: "login",
+        title: "Login",
+        component: LoginComponent,
     }
 ];
